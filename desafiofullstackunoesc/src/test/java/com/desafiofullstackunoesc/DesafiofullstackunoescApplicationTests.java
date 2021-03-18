5040a49e58c050dc48d2e2ff30e52daf02b96f69
NatanCavasin <natancavasin@hotmail.com>
2021-03-18 12:58:25 -0300

    Upload Basic API

---
+++ desafiofullstackunoesc/src/test/java/com/desafiofullstackunoesc/DesafiofullstackunoescApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiofullstackunoesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiofullstackunoescApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
