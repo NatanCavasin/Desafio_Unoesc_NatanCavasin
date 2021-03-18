9fa833b03b08d9c444491a6009d16ea622bbf64c
Natan Cavasin <consumoenergiaunoesc@gmail.com>
2021-03-18 12:47:30 -0300

    First Commit

---
+++ desafiofullstackunoesc/src/main/java/com/desafiofullstackunoesc/DesafiofullstackunoescApplication.java
@@ -0,0 +1,13 @@
+package com.desafiofullstackunoesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiofullstackunoescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiofullstackunoescApplication.class, args);
+	}
+
+}
