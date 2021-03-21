c7cd30464092cb9e90c825e80e00d7ddcfda4ed9
NatanCavasin <natancavasin@hotmail.com>
2021-03-21 16:32:58 -0300

    atualização das classes da pasta api

--- desafiofullstackunoesc/src/main/java/com/desafiofullstackunoesc/api/alunoController.java
+++ desafiofullstackunoesc/src/main/java/com/desafiofullstackunoesc/api/alunoController.java
@@ -1,4 +1,15 @@
 package com.desafiofullstackunoesc.api;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/aluno")
 public class alunoController {
+
+    @Secured({"ROLE_ALUNO"})
+    public String login(){
+        return "Bem vindo aluno";
+    }
 }
